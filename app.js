48a454250bbd8452dc9d4603251e84d6b6e825e4
Naheed Awan <nadsawan@yahoo.com>
2021-12-18 12:12:38 -0500

    1st

--- app.js
+++ app.js
@@ -1,5 +1,26 @@
 class Casino {
     // Write code here
+    constructor(name) {
+        this.name = name;
+        this.timesPlayed = 0;
+    }
+    playGame(betAmount) {
+        if (Math.random() > 0.5) {
+            console.log("Casino Wins")
+            this.timesPlayed ++
+        }
+        else{
+            console.log ("You Won!" +" "+ betAmount * (this.timesPlayed + 1) + "$ dollars" )
+            this.timesPlayed ++
+            betAmount * (this.timesPlayed + 1)
+        }
+        
+    }
+rollDie(d){
+    console.log ("you rolled a " + (Math.floor(Math.random()*d)+1)+ "!"
+    )
+}
+
 };
 
 // TESTS
@@ -11,20 +32,18 @@ myCasino.playGame(25);
 myCasino.playGame(35);
 
 // BONUS TESTS
-/*
-const myBonusCasino = new Casino("HackerU Bonus Casino", true);
-console.log(myBonusCasino);
-myBonusCasino.playGame(5);
-myBonusCasino.playGame(15);
-myBonusCasino.playGame(25);
-myBonusCasino.playGame(35);
-*/
-
+ 
+ const myBonusCasino = new Casino("HackerU Bonus Casino", true);
+ console.log(myBonusCasino);
+ myBonusCasino.playGame(5);
+ myBonusCasino.playGame(15);
+ myBonusCasino.playGame(25);
+ myBonusCasino.playGame(35);
 // Extra BONUS TESTS
-/*
+
 const myExtraBonusCasino = new Casino("HackerU Extra Bonus Casino", false);
 console.log(myExtraBonusCasino);
 myExtraBonusCasino.rollDie(6);
 myExtraBonusCasino.rollDie(20);
 myExtraBonusCasino.rollDie(100);
-*/
+
